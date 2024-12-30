a5e7c82fa6c748706eaa41137a1451329be35c19
Clotho-ex <yusufcanvar90@gmail.com>
2024-12-30 21:34:14 +0300

    Change path to pattern-ticket.svg

--- tailwind.config.js
+++ tailwind.config.js
@@ -29,7 +29,7 @@ export default {
         "gradient-1": "linear-gradient(90deg, #F37362 0%, #FFF 100%)",
         "gradient-2":
           "linear-gradient(140deg, hsl(248deg 70% 10%) 0%, hsl(251deg 70% 11%) 8%, hsl(254deg 69% 11%) 15%, hsl(256deg 69% 11%) 22%, hsl(259deg 69% 11%) 28%, hsl(262deg 70% 11%) 34%, hsl(265deg 70% 11%) 40%, hsl(268deg 71% 11%) 46%, hsl(271deg 73% 11%) 52%, hsl(275deg 74% 11%) 57%, hsl(278deg 76% 11%) 63%, hsl(281deg 78% 11%) 68%, hsl(284deg 81% 11%) 73%, hsl(288deg 84% 11%) 79%, hsl(291deg 87% 11%) 84%, hsl(294deg 91% 11%) 89%, hsl(297deg 95% 10%) 95%, hsl(300deg 100% 10%) 100%)",
-        "ticket-pattern": "url('src/assets/images/pattern-ticket.svg')",
+        "ticket-pattern": "url('/pattern-ticket.svg')",
       },
 
       borderImage: {
